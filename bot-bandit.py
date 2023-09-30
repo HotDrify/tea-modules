aa13984caf491737573dbf5485c9d5e949116fe7
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-09-30 09:22:21 +0300

    Update bot-bandit.py
    fix client

--- bot-bandit.py
+++ bot-bandit.py
@@ -45,7 +45,7 @@ class BotBanditMod(loader.Module):
                     await conv.ask(command)
                     await asyncio.sleep(1)
                 msg = await conv.get_response()
-                path = await client.download_media(msg, msg.photo.file_id + ".png")
+                path = await app.download_media(msg, msg.photo.file_id + ".png")
                 async with fsm.Conversation(app, "@TranslateIDrobot", purge = True) as conv: # OCR
                     conv.ask_media(path, "photo")
                     await asyncio.sleep(10)
